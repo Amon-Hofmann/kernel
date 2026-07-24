c6ea18816e43a17ab4b84fc3343a11b8a5a2f882
Amon Hofmann <ahofmann@hm.edu>
2026-07-24 17:25:56 +0200

    introduce helper functions to translate between addr and frame
    - some debugging
    - change bitmap indexing define name

--- src/pmm.c
+++ src/pmm.c
@@ -14,22 +14,30 @@
 #define FRAME_SIZE 4096U
 #define MAX_FRAMES (1024U * 1024U) /* 4 GiB / 4 KiB */
 
+// addresses from 0 to (FRAME_SIZE * MAX_FRAMES) = 2^32
+
 #define ALIGN_DOWN(x, a) ((x) & ~((a) - 1))
 #define ALIGN_UP(x, a)   (((x) + (a) - 1) & ~((a) - 1))
-#define BITMAP_MAX_INDEX ((MAX_FRAMES) / 32)
+#define BITMAP_LEN       ((MAX_FRAMES) / 32)  // 2^15
+#define BITMAP_MAX_INDEX (BITMAP_LEN - 1)
 
-static uint32_t bitmap[BITMAP_MAX_INDEX]; /* 128 KiB */
+// indices from 0 to BITMAP_LEN - 1 = 2^15
 
-static void KERNEL_UNUSED KERNEL_COLD print_bitmap(
-    KERNEL_UNUSED uintptr_t addr_start, KERNEL_UNUSED uintptr_t addr_stop) {
-    for (uint16_t addr = 0; addr < BITMAP_MAX_INDEX; addr++) {
-    }
+static uint32_t bitmap[BITMAP_LEN]; /* 128 KiB */
+
+KERNEL_UNUSED static uintptr_t frame_to_addr(uint16_t frame) {
+    return ((uintptr_t)frame) << 12;
 }
-static void KERNEL_UNUSED KERNEL_COLD print_bitmap_idx(uint16_t idx_start,
-                                                       uint16_t idx_stop) {
-    for (uint16_t index = idx_start;
-         index < idx_stop && index < BITMAP_MAX_INDEX; index++) {
-        serial_printf("%X: %lb\n", index, (unsigned long)bitmap[index]);
+KERNEL_UNUSED static uint16_t addr_to_frame(uintptr_t addr) {
+    return (uint16_t)(addr >> 12);
+}
+
+static void print_bitmap(uint16_t frame_start, uint16_t frame_stop) {
+    serial_writestring("Address     Frame    Bitmap Value \n");
+    for (uint16_t frame = frame_start; frame < frame_stop && frame < BITMAP_LEN;
+         frame++) {
+        serial_printf("%lX  %u:    %lb\n", (long unsigned)frame_to_addr(frame),
+                      frame, (unsigned long)bitmap[frame]);
     }
 }
 
@@ -44,6 +52,12 @@ KERNEL_UNUSED KERNEL_COLD static void test_alignment(void) {
 }
 
 static void mark_used(uintptr_t addr, uintptr_t len) {
+    if (addr > MAX_FRAMES * FRAME_SIZE) {
+        serial_writestring("addr to big\n");
+    }
+    if (addr + len > MAX_FRAMES * FRAME_SIZE) {
+        serial_writestring("addr + len to big\n");
+    }
     uintptr_t end_addr = addr + len;
     uintptr_t start_frame = ALIGN_DOWN(addr, FRAME_SIZE) / FRAME_SIZE;
     uintptr_t stop_frame = ALIGN_UP(end_addr, FRAME_SIZE) / FRAME_SIZE;
@@ -68,20 +82,31 @@ KERNEL_UNUSED static void mark_free(uintptr_t addr, uintptr_t len) {
     }
 }
 
-KERNEL_UNUSED static bool test_frame(uint16_t N) {
-    return bitmap[N / 32] & (1u << (N % 32));  // non-zero → in use
+KERNEL_UNUSED static bool is_frame_in_use_idx(uint16_t index) {
+    return bitmap[index / 32] & (1u << (index % 32));  // non-zero → in use
+}
+
+KERNEL_UNUSED static bool is_frame_in_use_(uintptr_t addr) {
+    uint16_t index = ALIGN_UP(addr, FRAME_SIZE) / FRAME_SIZE;
+    return bitmap[index / 32] & (1u << (index % 32));  // non-zero → in use
 }
 
 void pmm_init(KERNEL_UNUSED multiboot_info_t *mbi) {
-    mark_used(0, 3 * FRAME_SIZE + 10);
-    print_bitmap_idx(0, 10);
+    mark_used(10, 50 * FRAME_SIZE + 10);
+    print_bitmap(0, 10);
+    serial_writestring("\n");
 
-    mark_free(0, 3 * FRAME_SIZE + 10);
-    print_bitmap_idx(0, 10);
+    mark_free(10, 50 * FRAME_SIZE + 10);
+    print_bitmap(0, 10);
+    serial_writestring("\n");
 
-    serial_writestring("printing bitmap\n");
-    print_bitmap_idx(BITMAP_MAX_INDEX - 10, BITMAP_MAX_INDEX);
-    serial_writestring("done..\n");
+    mark_used(MAX_FRAMES * FRAME_SIZE - FRAME_SIZE, 5);
+    serial_printf("is %sin use\n",
+                  is_frame_in_use_idx(BITMAP_LEN) ? "" : "not ");
+
+    serial_writestring("max - 10\n");
+    print_bitmap(BITMAP_LEN - 10, BITMAP_LEN);
+    serial_writestring("done\n");
 
 #ifdef DEBUG
     test_alignment();
